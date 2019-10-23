686c142008703d36e955df149f177e6f8839efb5
Peter Hall <peter.hall@sage.com>
2019-10-23 11:01:57 -0400

    support roundtrip reservations

--- SwaCheckin.py
+++ SwaCheckin.py
@@ -1,3 +1,6 @@
+# SwaCheckin.py -c <confirmation #> -l <lastName> -f <firstName> [-e <email>]
+# or use: SwaCheckin.py --confirmation=<confirmation> --lastName=<lastName> --firstName=<firstName> [--email <email>]
+
 import sys
 import argparse
 import smtplib
@@ -15,14 +18,13 @@ REQ_HEADERS = {'Host': 'mobile.southwest.com', 'X-API-Key': 'l7xx0a43088fe625471
 
 
 def main(argv):
-    # SwaCheckin.py -c <confirmation #> -l <lastName> -f <firstName> [-e <email>]
-    # or use: SwaCheckin.py --confirmation=<confirmation> --lastName=<lastName> --firstName=<firstName> [--email <email>]
-
     help_text = f"""Check in to SWA flights exactly 24 hours in advance. After check-in, an 
 email is sent with an itinerary summary and URLs for all boarding passes in a reservation. 
 An error email is sent if check-in fails. Reservations with multiple passengers will have 
 all passengers checked in and included in the itinerary summary email. The name of any 
-passenger in the reservation will work. SMTP configuration path - {path.dirname(path.realpath(__file__))}/smtp.yml"""
+passenger in the reservation will work. For roundtrip reservations, all outbound and 
+return flights will be queued for check-in. 
+SMTP configuration path - {path.dirname(path.realpath(__file__))}/smtp.yml"""
 
     parser = argparse.ArgumentParser(description=help_text)
     rn = parser.add_argument_group('required named arguments')
@@ -100,6 +102,58 @@ def send_email(subject, text, recipient, email_config):
         print(f"failed to send email - {subject}\n{str(ex)}")
 
 
+# print flight status; return only flights to be checked in
+def flight_info_status_filter(flight_info):
+    print("\n---- list of all flights and their check-in eligibility ----")
+    filtered_info = []
+    for flight in flight_info:
+        print(f"{flight['header']} - {flight['departureInfo']}\n{flight['title']}")
+        departure_date = dateutil.parser.parse(flight['departureDateTime'])
+        time_delta = departure_date - datetime.now().astimezone(pytz.timezone("UTC"))
+        if time_delta <= timedelta(days=1) and time_delta >= timedelta(seconds=0):
+            print(f"departure in {time_delta} -- check-in will occur in a moment\n")
+            filtered_info.append(flight)
+        elif time_delta > timedelta(days=1):
+            print(f"check-in will occur at {departure_date - timedelta(days=1)} -- {time_delta - timedelta(days=1)} from now\n")
+            filtered_info.append(flight)
+        else:
+            print("this flight won't be checked in\n")
+    return filtered_info
+
+
+# start check-in and return response
+def start_checkin(flight):
+    # retry retrieve check-in if error response
+    for i in range(15, 0, -1):
+        ci_data_response = retrieve_checkin_data(confirmation, last_name, first_name)
+        # code key only present on an error
+        if 'code' in ci_data_response:
+            print(f'Retries remaining: {str(i - 1)} -- Check-in error -- error body from SWA below.')
+            print(ci_data_response['message'])
+            if not (i - 1):
+                print('check-in failed')
+                if email:
+                    send_email('check-in failed', f'{confirmation} {last_name} {first_name}', email, email_config())
+                break
+            sleep(0.5)
+        else:
+            # return check-in response
+            return checkin(confirmation, last_name, first_name, ci_data_response['checkInSessionToken'])
+
+
+# get boarding pass(es) text for passenger
+def boarding_pass_text(passenger):
+    b_pass_response = retrieve_boarding_pass(confirmation, last_name, first_name, passenger['travelerID'])
+    boarding_pass_text = f'Boarding passes for {passenger["name"]} \n'
+    for b_pass in b_pass_response['checkInRetrieveBoardingPassPage']['mobileBoardingPassViewPage']['mobileBoardingPassView']:
+        boarding_pass_text += (
+            f'    Flight {b_pass["flightNumber"]} {b_pass["originAirportDescription"]} to '
+            f'{b_pass["destinationAirportDescription"]} departing {b_pass["departureTime"]} '
+            f'- boarding position {b_pass["boardingGroup"]} {b_pass["boardingPosition"]}'
+            f'\n    Open on a mobile device: {b_pass["adaptiveLink"]}\n\n')
+    return boarding_pass_text
+
+
 if __name__ == "__main__":
     args = main(sys.argv[1:])
     confirmation = args['confirmation']
@@ -115,49 +169,28 @@ if __name__ == "__main__":
             send_email('reservation not found', f'{confirmation} {last_name} {first_name}', email, email_config())
         sys.exit(1)
 
-    flight_info = resv_response['viewReservationViewPage']['shareDetails']['flightInfo'][0]
-    departure_date = dateutil.parser.parse(flight_info['departureDateTime'])
-    print(f"{flight_info['header']} - {flight_info['departureInfo']}\n{flight_info['title']}")
-    time_delta = departure_date - datetime.now().astimezone(pytz.timezone("UTC"))
-    if time_delta <= timedelta(days=1) and time_delta >= timedelta(seconds=0):
-        print(f"departure in {time_delta} -- starting check-in")
-    elif time_delta > timedelta(days=1):
-        print(f"check-in will occur at {departure_date - timedelta(days=1)} -- {time_delta - timedelta(days=1)}")
-        sleep(time_delta.total_seconds() - timedelta(days=1).total_seconds())
-    else:
-        print("""this reservation doesn't look eligible for check-in, but I'll try anyway
-¯\\_(ツ)_/¯""")
+    filtered_flights = flight_info_status_filter(resv_response['viewReservationViewPage']['shareDetails']['flightInfo'])
 
-    # retry check-in if error response
-    for i in range(15, 0, -1):
-        ci_data_response = retrieve_checkin_data(confirmation, last_name, first_name)
-        if 'code' in ci_data_response:
-            print(f'Retries remaining: {str(i - 1)} -- Check-in error -- error body from SWA below.')
-            print(ci_data_response['message'])
-            if not (i - 1):
-                print('check-in failed')
-                if email:
-                    send_email('check-in failed', f'{confirmation} {last_name} {first_name}', email, email_config())
-                sys.exit(1)
-            sleep(0.5)
-        else:
-            break
-    checkin_token = ci_data_response['checkInSessionToken']
+    if(len(filtered_flights)):
+        for flight in filtered_flights:
+            print("\n---- check-in queue status ----")
+            print(f"{flight['header']} - {flight['departureInfo']}\n{flight['title']}")
+            departure_date = dateutil.parser.parse(flight['departureDateTime'])
+            time_delta = departure_date - datetime.now().astimezone(pytz.timezone("UTC"))
+            if time_delta <= timedelta(days=1) and time_delta >= timedelta(seconds=0):
+                print(f"departure in {time_delta} -- starting check-in\n")
+                checkin_response = start_checkin(flight)
+            elif time_delta > timedelta(days=1):
+                print(f"check-in will occur at {departure_date - timedelta(days=1)} -- {time_delta - timedelta(days=1)} from now\n")
+                sleep(time_delta.total_seconds() - timedelta(days=1).total_seconds())
+                checkin_response = start_checkin(flight)
+            else:
+                print("error: this flight doesn't look eligible for check-in")
 
-    checkin_response = checkin(confirmation, last_name, first_name, checkin_token)
+            boarding_pass_out = ''
+            for passenger in checkin_response['checkInConfirmationPage']['flights'][0]['passengers']:
+                boarding_pass_out += boarding_pass_text(passenger)
 
-    boarding_pass_out = ''
-    for passenger in checkin_response['checkInConfirmationPage']['flights'][0]['passengers']:
-        b_pass_response = retrieve_boarding_pass(confirmation, last_name, first_name, passenger['travelerID'])
-        boarding_pass_out += f'Boarding passes for {passenger["name"]} \n'
-        for b_pass in b_pass_response['checkInRetrieveBoardingPassPage']['mobileBoardingPassViewPage']['mobileBoardingPassView']:
-            boarding_pass_out += (
-                f'    Flight {b_pass["flightNumber"]} {b_pass["originAirportDescription"]} to '
-                f'{b_pass["destinationAirportDescription"]} departing {b_pass["departureTime"]} '
-                f'- boarding position {b_pass["boardingGroup"]} {b_pass["boardingPosition"]}'
-                f'\n    Open on a mobile device: {b_pass["adaptiveLink"]}\n\n')
-        boarding_pass_out += '\n\n'
-
-    if email:
-        send_email(confirmation, boarding_pass_out, email, email_config())
-    print(boarding_pass_out)
+            if email:
+                send_email(confirmation, boarding_pass_out, email, email_config())
+            print(boarding_pass_out)
